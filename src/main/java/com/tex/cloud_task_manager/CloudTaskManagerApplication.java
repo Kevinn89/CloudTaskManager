91fed6bf303fe5145aace14d2d94a3245f09bd79
Kevinn Mitchell <kevinnmitchell@mac.lan>
2026-05-01 19:56:48 -0500

    ignore .DS_Store

---
+++ src/main/java/com/tex/cloud_task_manager/CloudTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tex.cloud_task_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTaskManagerApplication.class, args);
+	}
+
+}
